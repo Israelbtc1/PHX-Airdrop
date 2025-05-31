286046b3a072c4104e04202185512d96f9f7cfc7
Israelbtc1 <israelfelix2008@gmail.com>
2025-05-31 12:03:31 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,24 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 
 // 🔁 Replace this config with your actual Firebase project's config
+const firebaseConfig = {// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+  apiKey: "AIzaSyDE_0X9PDWpc0YlRcmS6noNzOg_fYTkRHk",
+  authDomain: "phx-airdrop.firebaseapp.com",
+  projectId: "phx-airdrop",
+  storageBucket: "phx-airdrop.firebasestorage.app",
+  messagingSenderId: "539552570976",
+  
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+  
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
